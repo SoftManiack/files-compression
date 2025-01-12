35949cc3e27b15a2f7b1b73efa16ca627a860799
Suckhodoev Artem <fetelupercal@gmail.com>
2025-01-12 23:18:20 +0500

    byte to bit

--- internal/RLE.go
+++ internal/RLE.go
@@ -9,23 +9,34 @@ func LRECompression(data []byte) []byte {
 
 	var compressed []byte
 
-	var bits_data []bits
-	var bits_compressed []bits
+	var bits_data string
+	var bits_compressed string
+
+	for i := 0; i < len(data); i++ {
+
+		bits_data += byteToBits(data[i])
+	}
+
+	fmt.Println(data[0:8])
+	fmt.Println(bits_data[0])
+
+	//fmt.Println(bits_data)
 
 	fmt.Printf("uncompressed length %d \n", len(bits_data))
 
 	var count uint8 = 1
 	var value uint8 = 0
 
-	for i := 0; i < len(bits_data); i++ {
+	for i := 0; i < 10; i++ {
 
+		fmt.Println("*")
+		fmt.Println(bits_data[i])
 		if i == 0 {
 			value = bits_data[i]
 			count--
 		} else {
 			count++
-			bits_compressed = append(bits_compressed, value)
-			bits_compressed = append(bits_compressed, count)
+			bits_compressed = bits_compressed + string(value) + string(count)
 
 			value = bits_data[i]
 			count = 0
@@ -33,6 +44,7 @@ func LRECompression(data []byte) []byte {
 	}
 
 	fmt.Printf("compressed length %d \n", len(bits_compressed))
+	fmt.Println(bits_compressed)
 
 	return compressed
 
@@ -41,4 +53,16 @@ func LRECompression(data []byte) []byte {
 func LREDecompression(data []byte) []byte {
 
 	return nil
+}
+
+func byteToBits(b byte) string {
+	var bits string
+	for i := 7; i >= 0; i-- {
+		if (b & (1 << i)) != 0 {
+			bits += "1"
+		} else {
+			bits += "0"
+		}
+	}
+	return bits
 }
