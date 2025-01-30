84464c871387899a373f5ff53814b831e6a95a88
Suckhodoev Artem <fetelupercal@gmail.com>
2025-01-30 21:10:22 +0500

    convert to []byte

--- internal/RLE.go
+++ internal/RLE.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 func LRECompression(data []byte) []byte {
@@ -21,31 +22,55 @@ func LRECompression(data []byte) []byte {
 	fmt.Println(reflect.TypeOf(bits_data))
 	fmt.Println(data[0:8])
 	fmt.Println(reflect.TypeOf(bits_data[0]))
-	fmt.Println(string(bits_data[0]))
+	fmt.Println(string(bits_data[0:8]))
+	fmt.Println(string(bits_data[8:16]))
 
 	//fmt.Println(bits_data)
 
 	fmt.Printf("uncompressed length %d \n", len(bits_data))
 
-	var count uint8 = 1
+	var count int = 1
 	var value string = "0"
 
+	fmt.Println(bits_data[11:100])
 	for i := 0; i < len(bits_data); i++ {
+
+		//fmt.Print(string(bits_data[i]))
 
 		if i == 0 {
 			value = string(bits_data[i])
-			count--
-		} else if value != string(bits_data[i]) {
-			count++
-			bits_compressed = string(bits_compressed) + string(value) + string(count) + "-"
+			count += 1
+		} else {
+			if value != string(bits_data[i]) {
 
-			value = string(bits_data[i])
-			count = 0
+				if count > 9 {
+					bits_compressed = bits_compressed + "-" + value + strconv.Itoa(count) + "-"
+				} else {
+					bits_compressed = bits_compressed + value + strconv.Itoa(count)
+				}
+				value = string(bits_data[i])
+				count = 1
+
+			} else {
+				count += 1
+			}
 		}
+
+		//fmt.Println(string(bits_data[i]))
 	}
 
+	fmt.Println(bits_compressed[0:100])
+
 	fmt.Printf("compressed length %d \n", len(bits_compressed))
-	fmt.Println(bits_compressed)
+
+	// bit convert to byte
+
+	for _, n := range bits_compressed {
+
+		compressed = append(compressed, byte(n))
+	}
+
+	//fmt.Println(compressed[0:16])
 
 	return compressed
 
